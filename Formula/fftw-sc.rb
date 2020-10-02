eaeeafba86155083d4043df490aca52cbc35ddb2
Marcin Paczkowski <dyfeer@gmail.com>
2020-10-01 19:49:57 -0700

    Create fftw-sc.rb

---
+++ Formula/fftw-sc.rb
@@ -0,0 +1,55 @@
+class FftwSc < Formula
+  desc "C routines to compute the Discrete Fourier Transform"
+  homepage "http://www.fftw.org"
+  url "http://fftw.org/fftw-3.3.8.tar.gz"
+  sha256 "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303"
+  revision 1
+
+  keg_only "it is a shared library to be used specifically for building SuperCollider server, single precision only"
+
+  def install
+    args = [
+      "--enable-shared",
+      "--disable-debug",
+      "--prefix=#{prefix}",
+      "--enable-threads",
+      "--disable-dependency-tracking",
+      "--disable-fortran",
+    ]
+
+    # FFTW supports runtime detection of CPU capabilities, so it is safe to
+    # use with --enable-avx and the code will still run on all CPUs
+    simd_args = ["--enable-sse2", "--enable-avx"]
+
+    # single precision
+    # enable-sse2, enable-avx and enable-avx2 work for both single and double precision
+    # SuperCollider only uses single precision
+    system "./configure", "--enable-single", *(args + simd_args)
+    system "make", "install"
+
+  end
+
+  test do
+    # Adapted from the sample usage provided in the documentation:
+    # http://www.fftw.org/fftw3_doc/Complex-One_002dDimensional-DFTs.html
+    (testpath/"fftw.c").write <<~EOS
+      #include <fftw3.h>
+      int main(int argc, char* *argv)
+      {
+          fftw_complex *in, *out;
+          fftw_plan p;
+          long N = 1;
+          in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+          out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+          p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+          fftw_execute(p); /* repeat as needed */
+          fftw_destroy_plan(p);
+          fftw_free(in); fftw_free(out);
+          return 0;
+      }
+    EOS
+
+    system ENV.cc, "-o", "fftw", "fftw.c", "-L#{lib}", "-lfftw3", *ENV.cflags.to_s.split
+    system "./fftw"
+  end
+end
