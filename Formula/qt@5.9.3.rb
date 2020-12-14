f9fd277344285f353778f4bbcdae7c8568489a0a
Marcin Paczkowski <dyfeer@gmail.com>
2020-12-14 00:16:07 -0800

    Update qt@5.9.3.rb

--- Formula/qt@5.9.3.rb
+++ Formula/qt@5.9.3.rb
@@ -15,6 +15,8 @@ class QtAT593 < Formula
   bottle do
     cellar :any
     root_url "https://homebrew.bintray.com/bottles"
+    sha256 "570ca1b244dbcecc74d2b7813e5659024eba1500640c84b1b6eedcd96dd1ba6f" => :catalina # re-use high_sierra
+    sha256 "570ca1b244dbcecc74d2b7813e5659024eba1500640c84b1b6eedcd96dd1ba6f" => :mojave # re-use high_sierra
     sha256 "570ca1b244dbcecc74d2b7813e5659024eba1500640c84b1b6eedcd96dd1ba6f" => :high_sierra
     sha256 "a05e2f102433b8e0eee1c8cbd0f27949c9314f7ebf237766bcf191ffdb4b6940" => :sierra
     sha256 "42eae12f322119d24b695fdb29678b50b020dfde0a24bdd8caf4a952649ac2d0" => :el_capitan
